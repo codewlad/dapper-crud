6b54d0b31361bfd5423d8fd306a81e241a49ca7d
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-11-29 11:08:26 -0300

    create the project

---
+++ Livros/Services/LivrosService/ILivroInterface.cs
@@ -0,0 +1,13 @@
+﻿using Livros.Models;
+
+namespace Livros.Services.LivrosService
+{
+    public interface ILivroInterface
+    {
+        Task<IEnumerable<Livro>> GetAllLivros();
+        Task<Livro> GetLivroById(int livroId);
+        Task<IEnumerable<Livro>> CreateLivro(Livro livro);
+        Task<IEnumerable<Livro>> UpdateLivro(Livro livro);
+        Task<IEnumerable<Livro>> DeleteLivro(int livroId);
+    }
+}
---
+++ Livros/Services/LivrosService/LivroService.cs
@@ -0,0 +1,68 @@
+﻿using Dapper;
+using Livros.Models;
+using System.Data.SqlClient;
+
+namespace Livros.Services.LivrosService
+{
+    public class LivroService : ILivroInterface
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string? getConnection;
+        public LivroService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            getConnection = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<Livro>> CreateLivro(Livro livro)
+        {
+            using (var con = new SqlConnection(getConnection))
+            {
+                var sql = "insert into Livros(titulo, autor) values (@titulo,@autor)";
+                await con.ExecuteAsync(sql, livro);
+
+                return await con.QueryAsync<Livro>("select * from livros");
+            }
+        }
+
+        public async Task<IEnumerable<Livro>> DeleteLivro(int livroId)
+        {
+            using (var con = new SqlConnection(getConnection))
+            {
+                var sql = "delete from Livros where id = @Id";
+                await con.ExecuteAsync(sql, new { Id = livroId });
+
+                return await con.QueryAsync<Livro>("select * from livros");
+            }
+        }
+
+        public async Task<IEnumerable<Livro>> GetAllLivros()
+        {
+            using (var con = new SqlConnection(getConnection))
+            {
+                var sql = "select * from Livros";
+                return await con.QueryAsync<Livro>(sql);
+            }
+        }
+
+        public async Task<Livro> GetLivroById(int livroId)
+        {
+            using (var con = new SqlConnection(getConnection))
+            {
+                var sql = "select * from Livros where id = @Id";
+                return await con.QueryFirstOrDefaultAsync<Livro>(sql, new { Id = livroId });
+            }
+        }
+
+        public async Task<IEnumerable<Livro>> UpdateLivro(Livro livro)
+        {
+            using (var con = new SqlConnection(getConnection))
+            {
+                var sql = "update Livros set titulo = @titulo, autor = @autor where id = @id";
+                await con.ExecuteAsync(sql, livro);
+
+                return await con.QueryAsync<Livro>("select * from livros");
+            }
+        }
+    }
+}
